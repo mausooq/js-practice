486c0ad2d7d85a84273fe5353d2c19d5ccedffdc
mausooq <mauchoomausooq051@gmail.com>
2024-01-26 23:19:43 +0530

    async problem

---
+++ js_more/4.js
@@ -0,0 +1,6 @@
+// ### Array Filter with Callback:
+// Write a function 'filterArray' that takes an array and a callback function as arguments. 'filterArray' should filter the elements of the array based on the condition specified by the callback function and return a new array with the filtered elements.
+
+function filterArray(callback){
+    
+}
