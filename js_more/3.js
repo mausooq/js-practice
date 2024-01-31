223bd7cf941d2b643afc5700a87e35749597c7ad
mausooq <mauchoomausooq051@gmail.com>
2024-01-31 07:08:56 +0530

    new

--- js_more/3.js
+++ js_more/3.js
@@ -2,16 +2,16 @@
 // - Implement a function 'mapArray' that takes an array and a callback function as arguments. 'mapArray' should apply the callback function to each element of
 // the array and return a new array with the modified values.
 
-// function changes(a){
-//    return a*2;
-// }
-// function mapArray(arr,callback){
-//     let result = [];
-//     for (let i=0;i< arr.length ;i++){
-//         result[i] = callback(arr[i]);
-//     }
-//     return result
-// }
-// let arr=[1,2,3];
-// var res=mapArray(arr,changes)
-// console.log(res);  
+function changes(a){
+   return a*2;
+}
+function mapArray(arr,callback){
+    let result = [];
+    for (let i=0;i< arr.length ;i++){
+        result[i] = callback(arr[i]);
+    }
+    return result
+}
+let arr=[1,2,3];
+var res=mapArray(arr,changes)
+console.log(res);  
--- js_more/4.js
+++ js_more/4.js
@@ -1,6 +1,21 @@
 // ### Array Filter with Callback:
 // Write a function 'filterArray' that takes an array and a callback function as arguments. 'filterArray' should filter the elements of the array based on the condition specified by the callback function and return a new array with the filtered elements.
 
-function filterArray(callback){
-    
+function filterArray(arr,callback){
+    function xyz(element,index,array){
+        if(callback(element)===true)
+            return true;
+        else
+            return false
+    }
+    return arr.filter(xyz);
 }
+function isEven(element){
+    if(element%2 === 0){
+        return true;
+    }
+}
+
+let arr=[1,2,3,4,5,6,7,8,9,10];
+newArr = filterArray(arr,isEven);
+console.log(newArr);
